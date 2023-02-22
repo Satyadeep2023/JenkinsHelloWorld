7ee35c7347d313c5dc275610c2686994246dcc6e
Satyadeep2023 <125433274+Satyadeep2023@users.noreply.github.com>
2023-02-22 19:09:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ class HelloWorld
 // Prints "Hello, World" to the terminal window
 public static void main(String[] args)
 {
-System.out.println("Hello World & Welcome to Jenkins Training");
+System.out.println("Made some Changes in java file ");
 }
 }
